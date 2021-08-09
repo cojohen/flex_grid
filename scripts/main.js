8f692ecefbca12f37efa2895f608549ad50587bf
Joe Cohen <DeskOfJoe@gmail.com>
2021-08-08 19:54:02 -0500

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+/***********************************************************
+**
+**    Title:      main.js
+**    Project:    
+**    Author:     Joe Cohen
+**    Contact:    <deskofjoe@gmail.com>
+**    GitHub:     https://github.com/cojohen
+**    
+** 
+***********************************************************/
